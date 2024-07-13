f332e050433b9178d5d68097e71bbfd55f6f7992
Jakostein97 <jacques.floresgarcia@gmail.com>
2024-07-13 18:53:42 -0400

    adds routes to articleRoutes.js

---
+++ controllers/api/articleRoutes.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const Article = require('../../models/Article');
+
+router.get('/', async (req, res) => {
+  const articleData = await Article.findAll();
+
+  return res.json(articleData);
+});
+
+router.post('/', async (req, res) => {
+  const articleData = await Article.create(req.body);
+
+  return res.json(articleData);
+});
+
+router.get('/:id', async (req, res) => {
+  const articleData = await Article.findByPk(req.params.id);
+
+  return res.json(articleData);
+});
+
+router.put('/:id', async (req, res) => {
+  const articleData = await Article.update(
+    {
+      title: req.body.title,
+      author: req.body.author,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  );
+
+  return res.json(articleData);
+});
+
+router.delete('/:id', async (req, res) => {
+  const articleData = await Article.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  return res.json(articleData);
+});
+
+module.exports = router;
